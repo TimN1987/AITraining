ca8af98307fa4022b0841c06244c1fef8ef99fee
TimN1987 <timothy.naish@gmail.com>
2026-05-04 11:48:08 +0100

    Completed env and set up agent.

--- src/snap/env.py
+++ src/snap/env.py
@@ -4,19 +4,41 @@ import random
 class SnapEnv:
     def __init__(self, ai_player: RLPlayer):
         self.REWARDS = {
-            'snap': 10,
-            'mistake': -10
+            True: 10,
+            False: -10
         }
         self.CARDS = 'ABCDEFGH'
         self.CARD_COUNT = 4
 
         self.player = ai_player
         self.player_hand = []
-        self.reset()
 
     def reset(self):
         self.player_hand.clear()
         for x in self.CARDS:
             for _ in range(self.CARD_COUNT):
                 self.player_hand.append(x)
-        self.player_hand = random.shuffle(self.player_hand)
+        random.shuffle(self.player_hand)
+
+    def run_episode(self):
+        self.reset()
+        game_over = False
+        episode_history = []
+        prev = self.player_hand.pop()
+        while len(self.player_hand) > 0:
+            curr = self.player_hand.pop()
+            state = self.player.get_state(prev, curr)
+            choice = self.player.choose_action(state)
+            reward = self.calculate_reward(prev, curr, choice)
+            episode_history.append(
+                {
+                    'state': state,
+                    'action': choice,
+                    'reward': reward
+                }
+            )
+            prev = curr
+        return episode_history
+    
+    def calculate_reward(self, prev: str, curr: str, choice: bool):
+        return self.REWARDS[(prev == curr) == choice]
